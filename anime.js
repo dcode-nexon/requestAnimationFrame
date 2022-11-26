3f57392c59b63c9394a75863c08cb146f915c192
tubi55 <tubi55@nate.com>
2022-11-26 15:25:17 +0900

    complete함수 추가 및  현재 수치값 대비 모션적용

--- anime.js
+++ anime.js
@@ -1,32 +1,28 @@
-/*
-  performance.now();
-  브라우저가 로딩된 순간부터 해당구문이  호출된 시점까지의 시간을 ms단위로 반환
-  정밀한 시간계산이 필요할때 활용
-*/
-
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
 	anime(box, {
-		prop: 'margin-top',
-		value: 300,
-		duration: 500,
+		prop: 'margin-left',
+		value: 200,
+		duration: 1000,
 	});
 });
 
 function anime(selector, option) {
 	const startTime = performance.now();
-	requestAnimationFrame(move);
+	const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+	option.value !== currentValue && requestAnimationFrame(run);
 
-	function move(time) {
+	function run(time) {
 		const timelast = time - startTime;
 		let progress = timelast / option.duration;
 
 		progress < 0 && (progress = 0);
 		progress > 1 && (progress = 1);
-		progress < 1 && requestAnimationFrame(move);
+		progress < 1 ? requestAnimationFrame(run) : option.callback && option.callback();
 
-		selector.style[option.prop] = `${option.value * progress}px`;
+		let result = currentValue + (option.value - currentValue) * progress;
+		selector.style[option.prop] = `${result}px`;
 	}
 }
