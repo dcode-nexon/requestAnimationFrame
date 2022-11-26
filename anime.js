70edbe1fef12db4fdcc66d12a958a6220ba1e1f7
tubi55 <tubi55@nate.com>
2022-11-26 16:07:15 +0900

    opacity속성값 적용

--- anime.js
+++ anime.js
@@ -3,15 +3,16 @@ const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
 	anime(box, {
-		prop: 'top',
-		value: '50%',
+		prop: 'opacity',
+		value: 1,
 		duration: 1000,
 	});
 });
 
 function anime(selector, option) {
 	const startTime = performance.now();
-	let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+	//px, %, opacity값 모두 커버하기 위해서 parseFloat으로 속성값을 실수로 반환받음
+	let currentValue = parseFloat(getComputedStyle(selector)[option.prop]);
 
 	const isString = typeof option.value;
 	if (isString === 'string') {
@@ -37,6 +38,8 @@ function anime(selector, option) {
 
 		//isString값이 문자열이면 뒤에 %적용, 그렇지않으면 px적용
 		if (isString === 'string') selector.style[option.prop] = `${result}%`;
+		//만약 속성명이 opacity이면 실수값을 바로 적용
+		else if (option.prop === 'opacity') selector.style[option.prop] = result;
 		else selector.style[option.prop] = `${result}px`;
 	}
 }
