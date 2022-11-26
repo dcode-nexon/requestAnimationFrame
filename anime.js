b9220b9bbc772b76439433369160e0b06ecde64f
tubi55 <tubi55@nate.com>
2022-11-26 15:55:07 +0900

    퍼센트 연산처리

--- anime.js
+++ anime.js
@@ -3,15 +3,26 @@ const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
 	anime(box, {
-		prop: 'margin-left',
-		value: 200,
+		prop: 'top',
+		value: '50%',
 		duration: 1000,
 	});
 });
 
 function anime(selector, option) {
 	const startTime = performance.now();
-	const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+	let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+
+	const isString = typeof option.value;
+	if (isString === 'string') {
+		const parentW = parseInt(getComputedStyle(selector.parentElement).width);
+		const parentH = parseInt(getComputedStyle(selector.parentElement).height);
+		const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
+		const y = ['margin-top', 'margin-bottom', 'top', 'bottom', 'height'];
+		for (const cond of x) option.prop === cond && (currentValue = (currentValue / parentW) * 100);
+		for (const cond of y) option.prop === cond && (currentValue = (currentValue / parentH) * 100);
+		option.value = parseFloat(option.value);
+	}
 	option.value !== currentValue && requestAnimationFrame(run);
 
 	function run(time) {
@@ -23,6 +34,9 @@ function anime(selector, option) {
 		progress < 1 ? requestAnimationFrame(run) : option.callback && option.callback();
 
 		let result = currentValue + (option.value - currentValue) * progress;
-		selector.style[option.prop] = `${result}px`;
+
+		//isString값이 문자열이면 뒤에 %적용, 그렇지않으면 px적용
+		if (isString === 'string') selector.style[option.prop] = `${result}%`;
+		else selector.style[option.prop] = `${result}px`;
 	}
 }
